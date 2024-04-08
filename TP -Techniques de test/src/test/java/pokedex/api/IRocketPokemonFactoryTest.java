2b668c52266a1bdc9783eff8d1fb6185b9936350
AlexisSaccoman <116709408+AlexisSaccoman@users.noreply.github.com>
2024-04-08 03:36:18 +0200

    Update config

---
+++ TP -Techniques de test/src/test/java/pokedex/api/IRocketPokemonFactoryTest.java
@@ -0,0 +1,44 @@
+package pokedex.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pokedex.apiS.RocketPokemonFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class IRocketPokemonFactoryTest {
+    IPokemonFactory pokemonFactory = new RocketPokemonFactory();
+    Pokemon myBulbizarre;
+    Pokemon myAquali;
+
+
+    @BeforeEach
+    public void setUp() {
+        pokemonFactory = new RocketPokemonFactory();
+        myBulbizarre = new Pokemon(0, "Bulbizarre", 126,126,90,613,64,4000,4,56);
+        myAquali = new Pokemon(133,"Aquali",186,168,260,2729,202,5000,4,100);
+    }
+
+    @Test
+    public void testCreatePokemon() {
+        assertNotNull(pokemonFactory.createPokemon(0, 613, 64, 4000, 4));
+        assertNotNull(pokemonFactory.createPokemon(133, 2729, 202, 5000, 4));
+    }
+    @Test
+    public void TestGetHP() {
+        assertEquals(pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCp(), myBulbizarre.getCp());
+    }
+    @Test
+    public void TestGetCP(){
+        assertEquals(pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getHp(), myBulbizarre.getHp());
+    }
+    @Test
+    public void TestGetDust(){
+        assertEquals(pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getDust(), myBulbizarre.getDust());
+    }
+    @Test
+    public void TestGetCandy(){
+        assertEquals(pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCandy(), myBulbizarre.getCandy());
+    }
+}
--- TP -Techniques de test/target/site/apidocs/package-search-index.js
+++ TP -Techniques de test/target/site/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pokedex.api"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pokedex.api"},{"l":"pokedex.apiS"}];updateSearchResults();
